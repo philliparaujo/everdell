8f5d9bc9c89690ab1134c97cd351a0e84ecd50fa
unknown <phillip@lunenetworks.com>
2025-06-04 21:59:30 -0700

    Player's status always above opponent's

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { useGame } from "../engine/GameContext";
+import { getPlayerColor, getPlayerId, oppositePlayerOf } from "../engine/helpers";
 import { headingStyling, idStyle, sideBarStyling } from "../screens/Game";
 import Controls from "./Controls";
 import PlayerStatus from "./PlayerStatus";
@@ -10,14 +11,18 @@ function Sidebar({ gameId }: { gameId: string | undefined }) {
     game
   } = useGame();
 
+  const storedId = getPlayerId();
+  const playerColor = getPlayerColor(game, storedId);
+  const topStatusColor = playerColor ?? "Red";
+
   return (
     <div style={sideBarStyling}>
       <div style={{ padding: '8px' }}>
         <h3 style={{ ...headingStyling }}>{game.turn}'s Turn</h3>
         <hr />
-        <PlayerStatus playerColor={"Red"} />
+        <PlayerStatus playerColor={topStatusColor} />
         <hr />
-        <PlayerStatus playerColor={"Blue"} />
+        <PlayerStatus playerColor={oppositePlayerOf(topStatusColor)} />
         <hr />
         <Controls />
         <hr />
